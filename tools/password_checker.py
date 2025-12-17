1464a2adac2e85f1e2238fa9177458b2e43d7e84
Yug Bothra <yugbothra200@outlook.com>
2025-12-17 23:35:26 +0530

    Add special character check to password strength checker

--- tools/password_checker.py
+++ tools/password_checker.py
@@ -3,14 +3,16 @@ def check_password_strength(password):
     has_upper = any(char.isupper() for char in password)
     has_lower = any(char.islower() for char in password)
     has_digit = any(char.isdigit() for char in password)
+    has_special = any(not char.isalnum() for char in password)
 
 
-    score = sum([length_score, has_upper, has_lower, has_digit])
+
+    score = sum([length_score, has_upper, has_lower, has_digit, has_special])
 
 
     if score <= 2:
         return "Weak"
-    elif score == 3:
+    elif score == 4:
         return "Medium"
     else:
         return "Strong"
