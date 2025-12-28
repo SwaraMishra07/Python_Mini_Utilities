e2f374f47885786718a3c8a0b2ca1d0de8483f11
Keerthika Ch <cse23733002@matrusri.edu.in>
2025-12-28 23:30:16 +0530

    Added 3 new quotes the list.

--- tools/quote_generator.py
+++ tools/quote_generator.py
@@ -3,7 +3,10 @@ QUOTES = [
 "Code is like humor. When you have to explain it, it’s bad.",
 "First, solve the problem. Then, write the code.",
 "Experience is the name everyone gives to their mistakes.",
-"In theory, theory and practice are the same. In practice, they’re not."
+"In theory, theory and practice are the same. In practice, they’re not.",
+"Debugging is twice as hard as writing the code in the first place.",
+"Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+"If debugging is the process of removing bugs, then programming must be the process of putting them in."
 ]
 
 if __name__ == "__main__":
