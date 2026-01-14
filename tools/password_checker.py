298ac9cd367ad74cc5223235060c7a2e3028e6a8
rudrika778 <trudrika@gmail.com>
2026-01-14 09:41:50 +0530

    Feat: mark password as strong if it contains a symbol

--- tools/password_checker.py
+++ tools/password_checker.py
@@ -1,4 +1,5 @@
 def check_password_strength(password):
+    symbols="!@#$%^&*()_-+={}[]|/~`"
     length_score = len(password) >= 8
     #Empty or whitespaces check
     if not password or not password.strip():
@@ -11,12 +12,16 @@ def check_password_strength(password):
     #Password must be of minimum 8 charachters
     if len(password)<8:
         print("Password must of minimum 8 characters!")
-        
+
+
     has_upper = any(char.isupper() for char in password)
     has_lower = any(char.islower() for char in password)
     has_digit = any(char.isdigit() for char in password)
+    
+    #Adding symbol check in Password
+    has_symbol= any(char in symbols for char in password)
 
-    score = sum([length_score, has_upper, has_lower, has_digit])
+    score = sum([length_score, has_upper, has_lower, has_digit, has_symbol])
 
 
     if score <= 2:
