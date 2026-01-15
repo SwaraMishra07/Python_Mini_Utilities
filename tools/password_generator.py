c68fa436828759bf4d3412c0991fec2e21b6dde0
harsh <harshtpo13@gmail.com>
2026-01-15 14:18:28 +0530

    used secrets module instead of random for better security in password generation.

--- tools/password_generator.py
+++ tools/password_generator.py
@@ -1,14 +1,14 @@
-import random
+import secrets as sc
 import string
 
 def generate_password():
     l = string.ascii_lowercase
     u= string.ascii_uppercase
     d = string.digits
-    s = "!@#$%^&*()_-+="
+    n = "!@#$%^&*()_-+="
 
-    random_chars = random.choices(l, k=random.randint(3,5)) + random.choices(u,k=random.randint(3,5)) + random.choices(d,k=random.randint(3,5)) + random.choices(s,k=random.randint(3,5))
-    random.shuffle(random_chars)
+    random_chars =  [sc.choice(l) for i in range(3+sc.randbelow(3))] + [sc.choice(u) for i in range(3+sc.randbelow(3))] + [sc.choice(n) for i in range(2+sc.randbelow(3))] + [sc.choice(d) for i in range(2+sc.randbelow(3))]
+    sc.SystemRandom().shuffle(random_chars)
     password = ''.join(random_chars)
     return password
 
